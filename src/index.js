b425f495e517a82269571325a08020f6b65fe9f0
Jeremy Bilic <jeremy.bilic@gmail.com>
2019-11-18 13:39:30 -0700

    Created intial commit for routing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './client/index.css';
-import App from './client/App';
-import * as serviceWorker from './client/serviceWorker';
+import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
+
+//Import desktop pages 
+import HomePage from './pages/home';
+
+
+export default class App extends React.Component {
+    render = () => {
+        return (
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={() => <HomePage />} />
+                    <Route exact path="/home" component={() => <HomePage />} />
+                </Switch>
+            </Router>
+        );
+    }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//serviceWorker.unregister();
