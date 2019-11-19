924ec5448d57434606f52b6279841874970e6f1c
Nathan Chua <nchua235@mtroyal.ca>
2019-11-18 23:37:36 -0700

    fixed background image overlay

--- src/components/showcase.js
+++ src/components/showcase.js
@@ -6,15 +6,13 @@ export default class Showcase extends Component {
         return (
             <header id="showcase">
                 <div className="showcase-content">
-                    <h1 class="l-heading">
+                    <h1 className="l-heading">
                         The Future of <br/>Farm Management
-                </h1>
+                    </h1>
                     <div className="btn-container">
                         <a href="" className="btn lead">I Am a Contractor</a>
                         <a href="" className="btn lead">I Am a Farmer</a>
                     </div>
-
-
                 </div>
             </header>
         )
